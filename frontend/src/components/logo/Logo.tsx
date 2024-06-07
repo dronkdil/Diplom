a399a06925fba8f756a7cf94aa0e9c591dadcbd7
dronkdil <dimozplayer222@gmail.com>
2024-06-07 10:25:36 +0300

    add logo image

--- frontend/src/components/logo/Logo.tsx
+++ frontend/src/components/logo/Logo.tsx
@@ -1,11 +1,13 @@
 import { Routes } from '@/lib/routes.constants'
+import Image from 'next/image'
 import Link from 'next/link'
+import LogoImage from '../../../public/images/Logo.jpg'
 import styles from './Logo.module.scss'
 
 const Logo = () => {
   return (
     <Link href={Routes.Courses} className={styles.logo}>
-      <div className={styles.logo__cube}></div>
+      <Image src={LogoImage.src} width={40} height={40} alt="logo" className={styles.logo__cube} />
       <span className={styles.logo__text}>StudyEra</span>
     </Link>
   )
